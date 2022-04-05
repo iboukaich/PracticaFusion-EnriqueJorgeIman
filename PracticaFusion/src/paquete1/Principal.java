54e568a3dc49b93e0bd1975d99e13b0773ba7c06
Iman BT <iman.boukaich-talai@iessampedro.org>
2022-04-05 18:07:10 +0200

    fusion realizada

--- PracticaFusion/src/paquete1/Principal.java
+++ PracticaFusion/src/paquete1/Principal.java
@@ -2,9 +2,7 @@ package paquete1;
 
 public class Principal {
 	public static void main(String[] args) {
-	Pez darwin = new Pez ("Darwin", "naraja");
-		
-
+		Pez darwin = new Pez ("Darwin", "naraja");
 		Gato gato0 = new Gato("Michi", "blanco");
 
 		Tortuga tortuguita = new Tortuga ("Jaime",3);
