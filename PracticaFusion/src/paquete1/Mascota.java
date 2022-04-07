552efac4197fe5c3bc1d13ba584c7262dc8b1a0f
'quique' <'quique@correo.org'>
2022-04-07 17:23:32 +0200

    Atributo de castado

--- PracticaFusion/src/paquete1/Mascota.java
+++ PracticaFusion/src/paquete1/Mascota.java
@@ -2,6 +2,7 @@ package paquete1;
 
 public class Mascota {
 	private String nombre;
+	private boolean castrado;
 
 	public Mascota(String nombre) {
 		this.nombre = nombre;
@@ -18,6 +19,11 @@ public class Mascota {
 
 	public void MandarMensaje() {
 		System.out.println("Soy " + this.nombre + " y te voy a contar algo o no");
+		if (this.castrado=true) {
+			System.out.println("AUCH :(");
+		}else {
+			System.out.println("Menos mal");
+		}
 	}
 
 }
